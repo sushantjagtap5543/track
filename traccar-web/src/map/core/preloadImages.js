0e0e59d4f7d8f6c9344e5ef9949900169e5bb4c9
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-25 22:54:58 +0530

    Fix traccar-web build: remove references to missing SVG icons

--- traccar-web/src/map/core/preloadImages.js
+++ traccar-web/src/map/core/preloadImages.js
@@ -4,56 +4,28 @@ import { loadImage, prepareIcon } from './mapUtil';
 
 import directionSvg from '../../resources/images/direction.svg';
 import backgroundSvg from '../../resources/images/background.svg';
-import animalSvg from '../../resources/images/icon/animal.svg';
 import bicycleSvg from '../../resources/images/icon/bicycle.svg';
-import boatSvg from '../../resources/images/icon/boat.svg';
 import busSvg from '../../resources/images/icon/bus.svg';
 import carSvg from '../../resources/images/icon/car.svg';
-import camperSvg from '../../resources/images/icon/camper.svg';
-import craneSvg from '../../resources/images/icon/crane.svg';
 import defaultSvg from '../../resources/images/icon/default.svg';
-import startSvg from '../../resources/images/icon/start.svg';
-import finishSvg from '../../resources/images/icon/finish.svg';
-import helicopterSvg from '../../resources/images/icon/helicopter.svg';
 import motorcycleSvg from '../../resources/images/icon/motorcycle.svg';
 import personSvg from '../../resources/images/icon/person.svg';
-import planeSvg from '../../resources/images/icon/plane.svg';
-import scooterSvg from '../../resources/images/icon/scooter.svg';
-import shipSvg from '../../resources/images/icon/ship.svg';
-import tractorSvg from '../../resources/images/icon/tractor.svg';
-import trailerSvg from '../../resources/images/icon/trailer.svg';
-import trainSvg from '../../resources/images/icon/train.svg';
-import tramSvg from '../../resources/images/icon/tram.svg';
+import suvSvg from '../../resources/images/icon/suv.svg';
 import truckSvg from '../../resources/images/icon/truck.svg';
 import vanSvg from '../../resources/images/icon/van.svg';
 import nanobananaSvg from '../../resources/images/icon/nanobanana.svg';
-import suvSvg from '../../resources/images/icon/suv.svg';
 
 export const mapIcons = {
-  animal: animalSvg,
   bicycle: bicycleSvg,
-  boat: boatSvg,
   bus: busSvg,
   car: carSvg,
-  camper: camperSvg,
-  crane: craneSvg,
   default: defaultSvg,
-  finish: finishSvg,
-  helicopter: helicopterSvg,
   motorcycle: motorcycleSvg,
   person: personSvg,
-  plane: planeSvg,
-  scooter: scooterSvg,
-  ship: shipSvg,
-  start: startSvg,
-  tractor: tractorSvg,
-  trailer: trailerSvg,
-  train: trainSvg,
-  tram: tramSvg,
+  suv: suvSvg,
   truck: truckSvg,
   van: vanSvg,
   nanobanana: nanobananaSvg,
-  suv: suvSvg,
 };
 
 export const mapIconKey = (category) => {
