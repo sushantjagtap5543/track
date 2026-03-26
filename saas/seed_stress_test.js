5d7a077cb639eaada3d789ed6c7931cbae75742d
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-26 18:31:45 +0530

    GeoSurePath Dual-Sync Sovereign Seeder (Bcrypt Library Fix) deployed

--- saas/seed_stress_test.js
+++ saas/seed_stress_test.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const axios = require('axios');
 
 async function seedStressTest() {
