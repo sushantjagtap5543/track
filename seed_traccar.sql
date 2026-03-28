111e7c478839f09417e0f8e08f4a079d2d925021
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-28 18:13:36 +0530

    Add untracked sql and json files

---
+++ seed_traccar.sql
@@ -0,0 +1,2 @@
+INSERT INTO tc_users (name, email, hashedpassword, salt, administrator, latitude, longitude, zoom)
+VALUES ('Admin', 'admin@geosurepath.com', '66050C30A0E0D6FC7E58C4F6704ED2B7347953264F6CC88B', '8C6546305C2932E48614210B039A36444AB5374465ED1131', true, 0, 0, 0);
